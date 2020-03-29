737836aeecb7aee4185c6423010c0d1dfd7dc298
Vince <vincentguan23@gmail.com>
2020-03-29 14:09:08 -0700

    commit

--- stpete.py
+++ stpete.py
@@ -5,9 +5,8 @@ import numpy as np
 import math
 
 
-
 def run_simulation(batches, batch_size):
-    global total_payout, max_payout, batch_payout, max_payout_b, batch_payouts, max_payout_bs
+    global total_payout, max_payout, batch_payout, max_payout_b, batch_payouts, max_payout_bs, good_batches
     batch_payouts = arr.array('d', [])
     max_payout_bs = arr.array('d', [])
     for i in range(batches):
@@ -17,20 +16,24 @@ def run_simulation(batches, batch_size):
             run_game()
         total_payout = total_payout + batch_payout
         batch_payouts.append(batch_payout)
+        if (abs(math.log2(batch_size) / 2 - batch_payout/batch_size)) <= 0.5*math.log2(batch_size):
+            good_batches = good_batches + 1
         max_payout_bs.append(max_payout_b)
         if max_payout_b > max_payout:
             max_payout = max_payout_b
-    make_plots(max_payout_bs, batch_payouts)
+    make_plots(batch_payouts)
+    print("Good batch percentage:", good_batches*100/batches)
     return
 
-def make_plots(max_payout_bs, batch_payouts):
+
+def make_plots(batch_payouts):
     avg_batch_payouts = np.divide(batch_payouts, batch_size)
     plt.plot(np.arange(batches), avg_batch_payouts)
     plt.ylabel('average payout per game ($)')
-    plt.axhline(math.log2(batch_size)/2)
+    estimate = math.log2(batch_size) / 2 + math.log2(math.log2(batch_size))/2
+    plt.axhline(estimate)
     plt.show()
     return
-
 
 
 def run_game():
@@ -50,14 +53,15 @@ def run_game():
     return
 
 
-#click to run
 if __name__ == "__main__":
     batches = int(input('How many batches?'))
     batch_size = int(input('How many games per batch?'))
+    good_batches = 0
     games_played = 0
     total_payout = 0
     max_payout = 0
     run_simulation(batches, batch_size)
+    print(total_payout/(batch_size*batches), math.log2(batch_size)/2)
 
 
 
